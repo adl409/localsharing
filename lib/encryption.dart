5c85e959acb62e7e897640296c00df7ed52c4618
Aaron Le <46aaronle@gmail.com>
2024-07-26 14:22:15 -0500

    encryption

--- lib/encryption.dart
+++ lib/encryption.dart
@@ -8,16 +8,16 @@ class EncryptionHelper {
   final encrypt.IV _iv;
 
   EncryptionHelper(String key, String iv)
-      : _encrypter = encrypt.Encrypter(encrypt.AES(encrypt.Key.fromUtf8(key))),
-        _iv = encrypt.IV.fromUtf8(iv);
+      : _encrypter = encrypt.Encrypter(encrypt.AES(encrypt.Key.fromBase64(key))),
+        _iv = encrypt.IV.fromBase64(iv);
 
   Uint8List encryptData(Uint8List data) {
     final encrypted = _encrypter.encryptBytes(data, iv: _iv);
     return encrypted.bytes;
   }
 
-  List<int> decryptData(Uint8List data) {
+  Uint8List decryptData(Uint8List data) {
     final decrypted = _encrypter.decryptBytes(encrypt.Encrypted(data), iv: _iv);
-    return decrypted;
+    return Uint8List.fromList(decrypted); // Convert to Uint8List
   }
 }
